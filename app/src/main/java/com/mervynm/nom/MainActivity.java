d0986b7b11ea8a607d22a37242a3bd7f1514176f
Mervyn Mathew <mmervyn@fb.com>
2020-07-14 11:29:19 -0400

    Added skeleton bottom nav view to MainActivity

--- app/src/main/java/com/mervynm/nom/MainActivity.java
+++ app/src/main/java/com/mervynm/nom/MainActivity.java
@@ -1,14 +1,35 @@
 package com.mervynm.nom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupVariables();
+        setUpBottomNavigationBar();
+    }
+
+    private void setupVariables() {
+        bottomNavigationBar = findViewById(R.id.bottom_navigation);
+    }
+
+    private void setUpBottomNavigationBar() {
+        bottomNavigationBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return true;
+            }
+        });
     }
 }
