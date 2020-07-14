f7cff08472da037d827fd4a9ad971d0d1bfa6bd2
Mervyn Mathew <mmervyn@fb.com>
2020-07-14 17:58:55 -0400

    Skeleton MoreInformationComposeFragment navigates to HomeFragment

--- app/src/main/java/com/mervynm/nom/fragments/MoreInformationComposeFragment.java
+++ app/src/main/java/com/mervynm/nom/fragments/MoreInformationComposeFragment.java
@@ -1,5 +1,6 @@
 package com.mervynm.nom.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,10 +10,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mervynm.nom.R;
 
 public class MoreInformationComposeFragment extends Fragment {
+
+    EditText editTextLocation;
+    EditText editTextRecipe;
+    EditText editTextPrice;
+    Button buttonPost;
 
     public MoreInformationComposeFragment() {}
 
@@ -29,5 +37,29 @@ public class MoreInformationComposeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setupVariables(view);
+        setupOnClickListener();
     }
+
+    private void setupVariables(View view) {
+        editTextLocation = view.findViewById(R.id.editTextLocation);
+        editTextRecipe = view.findViewById(R.id.editTextRecipe);
+        editTextPrice = view.findViewById(R.id.editTextPrice);
+        buttonPost = view.findViewById(R.id.buttonPost);
+    }
+
+    private void setupOnClickListener() {
+        buttonPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToHome();
+            }
+        });
+    }
+
+    private void goToHome() {
+        assert getFragmentManager() != null;
+        getFragmentManager().beginTransaction().replace(R.id.frameLayoutContainer, new HomeFragment()).commit();
+    }
+
 }
