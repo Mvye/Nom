48eca0d4efcc906ea0317ef211d9d8730eee9e7d
Mervyn Mathew <mmervyn@fb.com>
2020-07-14 10:47:15 -0400

    Created a skeleton LoginActivity

---
+++ app/src/main/java/com/mervynm/nom/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.mervynm.nom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText editTextUsername;
+    EditText editTextPassword;
+    Button buttonLogin;
+    Button buttonSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setupVariables();
+        setOnClickListeners();
+    }
+
+    private void setupVariables() {
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+    }
+
+    private void setOnClickListeners() {
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToMainActivity();
+            }
+        });
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToMainActivity();
+            }
+        });
+    }
+
+    private void goToMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
--- app/src/main/java/com/mervynm/nom/ParseApplication.java
+++ app/src/main/java/com/mervynm/nom/ParseApplication.java
@@ -13,27 +13,19 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        // Use for troubleshooting -- remove this line for production
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
 
-        // Use for monitoring Parse OkHttp traffic
-        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
-        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
-        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         builder.networkInterceptors().add(httpLoggingInterceptor);
 
-        // set applicationId, and server server based on the values in the Heroku settings.
-        // clientKey is not needed unless explicitly configured
-        // any network interceptors must be added with the Configuration Builder given this syntax
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("mervyn-nom") // should correspond to APP_ID env variable
-                .clientKey("fbu2020NomMM")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .applicationId("mervyn-nom")
+                .clientKey("fbu2020NomMM")
                 .server("https://mervyn-nom.herokuapp.com/parse").build());
 
-        /*ParseObject testObject = new ParseObject("TestObject");
+        ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
-        testObject.saveInBackground();*/
+        testObject.saveInBackground();
     }
 }
