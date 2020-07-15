4af87ca0106fc5cea0fe2700ce29072a04bd472f
Mervyn Mathew <mmervyn@fb.com>
2020-07-15 17:42:04 -0400

    Created PostAdapter

--- app/src/main/java/com/mervynm/nom/adapters/PostAdapter.java
+++ app/src/main/java/com/mervynm/nom/adapters/PostAdapter.java
@@ -1,4 +1,104 @@
 package com.mervynm.nom.adapters;
 
-public class PostAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.Target;
+import com.mervynm.nom.R;
+import com.mervynm.nom.models.Post;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
+
+    Context context;
+    List<Post> posts;
+
+    public PostAdapter(Context context, List<Post> posts) {
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View postView = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(postView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    public void clear() {
+        posts.clear();
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<Post> postList) {
+        posts.addAll(postList);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageViewProfilePicture;
+        TextView textViewUsername;
+        ImageView imageViewPostImage;
+        ImageView imageViewLike;
+        ImageView imageViewLocation;
+        ImageView imageViewPrice;
+        ImageView imageViewRecipe;
+        TextView textViewLikeAmount;
+        TextView textViewUsername2;
+        TextView textViewDescription;
+        TextView textViewCreatedAt;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewProfilePicture = itemView.findViewById(R.id.imageViewProfilePicture);
+            textViewUsername = itemView.findViewById(R.id.textViewUsername);
+            imageViewPostImage = itemView.findViewById(R.id.imageViewPostImage);
+            imageViewLike = itemView.findViewById(R.id.imageViewLike);
+            imageViewLocation = itemView.findViewById(R.id.imageViewLocation);
+            imageViewPrice = itemView.findViewById(R.id.imageViewPrice);
+            imageViewRecipe = itemView.findViewById(R.id.imageViewRecipe);
+            textViewLikeAmount = itemView.findViewById(R.id.textViewLikeAmount);
+            textViewUsername2 = itemView.findViewById(R.id.textViewUsername2);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+            textViewCreatedAt = itemView.findViewById(R.id.textViewCreatedAt);
+        }
+
+        public void bind(Post post) {
+            imageViewProfilePicture.setVisibility(View.INVISIBLE);
+            String username = post.getAuthor().getUsername();
+            textViewUsername.setText(username);
+            Glide.with(context).load(post.getImage().getUrl())
+                               .override(Target.SIZE_ORIGINAL)
+                               .into(imageViewPostImage);
+            imageViewLocation.setVisibility(View.GONE);
+            if (post.getPrice() == 0) {
+                imageViewPrice.setVisibility(View.GONE);
+            }
+            textViewLikeAmount.setText("15 Likes");
+            textViewUsername2.setText(username);
+            textViewDescription.setText(post.getDescription());
+            textViewCreatedAt.setText(post.getCreatedAt().toString());
+        }
+    }
 }
--- app/src/main/java/com/mervynm/nom/fragments/HomeFragment.java
+++ app/src/main/java/com/mervynm/nom/fragments/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
@@ -14,17 +15,21 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.mervynm.nom.R;
+import com.mervynm.nom.adapters.PostAdapter;
 import com.mervynm.nom.models.Post;
 import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     RecyclerView recyclerViewPosts;
+    List<Post> posts;
+    PostAdapter adapter;
 
     public HomeFragment() {}
 
@@ -42,11 +47,19 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setupVariables(view);
+        setupRecyclerView();
         queryPosts();
     }
 
     private void setupVariables(View view) {
         recyclerViewPosts = view.findViewById(R.id.recyclerViewPosts);
+    }
+
+    private void setupRecyclerView() {
+        posts = new ArrayList<>();
+        adapter = new PostAdapter(getContext(), posts);
+        recyclerViewPosts.setAdapter(adapter);
+        recyclerViewPosts.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     private void queryPosts() {
@@ -55,14 +68,16 @@ public class HomeFragment extends Fragment {
         query.addDescendingOrder(Post.KEY_CREATED_AT);
         query.findInBackground(new FindCallback<Post>() {
             @Override
-            public void done(List<Post> posts, ParseException e) {
+            public void done(List<Post> postList, ParseException e) {
                 if (e != null) {
                     Log.e("Homefragment", "Issue with getting posts", e);
                     return;
                 }
-                for (Post post : posts) {
-                    Log.i("Homefragment", "Post " + post.getDescription() +  ", username " + post.getAuthor().getUsername());
+                for (Post post : postList) {
+                    Log.i("Homefragment", "Post " + post.getDescription() +  ", username " + post.getAuthor().getUsername() + " ," + post.getPrice());
                 }
+                adapter.clear();
+                adapter.addAll(postList);
             }
         });
     }
