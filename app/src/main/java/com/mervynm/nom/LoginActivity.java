619872df2b62cf6e5a18788585ed497b5fde3d8b
Mervyn Mathew <mmervyn@fb.com>
2020-07-16 10:37:55 -0400

    LoginActivity can now login already created users

--- app/src/main/java/com/mervynm/nom/LoginActivity.java
+++ app/src/main/java/com/mervynm/nom/LoginActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -34,13 +40,33 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToMainActivity();
+                login();
             }
         });
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToMainActivity();
+            }
+        });
+    }
+
+    private void login() {
+        String username = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+        loginUser(username, password);
+    }
+
+    private void loginUser(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Toast.makeText(LoginActivity.this, "Issue with Login", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                goToMainActivity();
+                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
             }
         });
     }
