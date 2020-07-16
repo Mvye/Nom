ef01c606af3691469c34469d4b1a1e904de66d67
Mervyn Mathew <mmervyn@fb.com>
2020-07-16 16:46:01 -0400

    Converted many fragments and activities to using Material buttons/textviews/toolbars

--- app/src/main/java/com/mervynm/nom/fragments/ComposeFragment.java
+++ app/src/main/java/com/mervynm/nom/fragments/ComposeFragment.java
@@ -1,11 +1,19 @@
 package com.mervynm.nom.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,14 +24,24 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import com.mervynm.nom.R;
+import com.mervynm.nom.external.BitmapScaler;
+
+import java.io.File;
+import java.util.Objects;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ComposeFragment extends Fragment {
 
+    public static final String TAG = "ComposeFragment";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 72;
     ImageView imageViewPicture;
     Button buttonTakePicture;
     EditText editTextDescription;
     Switch switchHomemade;
     Button buttonNext;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
 
     public ComposeFragment() {}
 
@@ -56,7 +74,7 @@ public class ComposeFragment extends Fragment {
         buttonTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "this is supposed to open the camera", Toast.LENGTH_SHORT).show();
+                launchCamera();
             }
         });
         buttonNext.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +83,40 @@ public class ComposeFragment extends Fragment {
                 goToMoreInformationComposeFragment();
             }
         });
+    }
+
+    private void launchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        photoFile = getPhotoFileUri(photoFileName);
+        Uri fileProvider = FileProvider.getUriForFile(Objects.requireNonNull(getContext()), "com.mervynm.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    private File getPhotoFileUri(String fileName) {
+        File mediaStorageDir = new File(Objects.requireNonNull(getContext()).getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                //Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                Uri takenPhotoUri = Uri.fromFile(getPhotoFileUri(photoFileName));
+                Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+                Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(rawTakenImage, 400);
+
+                imageViewPicture.setImageBitmap(resizedBitmap);
+            } else {
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void goToMoreInformationComposeFragment() {
