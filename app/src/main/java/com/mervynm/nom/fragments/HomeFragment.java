e4b6f32bbfc014c659c03ee758c553af496f864b
Mervyn Mathew <mmervyn@fb.com>
2020-07-23 15:41:48 -0400

    Users now only see posts from who they're following

--- app/src/main/java/com/mervynm/nom/fragments/HomeFragment.java
+++ app/src/main/java/com/mervynm/nom/fragments/HomeFragment.java
@@ -9,6 +9,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,9 +22,12 @@ import com.mervynm.nom.adapters.PostAdapter;
 import com.mervynm.nom.models.Location;
 import com.mervynm.nom.models.Post;
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +35,7 @@ import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
+    SwipeRefreshLayout swipeContainer;
     RecyclerView recyclerViewPosts;
     List<Post> posts;
     PostAdapter adapter;
@@ -50,8 +55,24 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setupSwipeRefreshLayout(view);
         setupRecyclerView(view);
         queryPosts();
+    }
+
+    private void setupSwipeRefreshLayout(View view) {
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                queryPosts();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
     private void setupRecyclerView(View view) {
@@ -101,22 +122,39 @@ public class HomeFragment extends Fragment {
     }
 
     protected void queryPosts() {
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_AUTHOR);
-        query.setLimit(20);
-        query.addDescendingOrder(Post.KEY_CREATED_AT);
-        query.findInBackground(new FindCallback<Post>() {
+        final List<ParseQuery<Post>> followingUsersPosts = new ArrayList<>();
+        final List<ParseUser> usernames = new ArrayList<>();
+        ParseQuery<ParseObject> following = ParseUser.getCurrentUser().getRelation("following").getQuery();
+        following.include("User");
+        following.findInBackground(new FindCallback<ParseObject>() {
             @Override
-            public void done(List<Post> postList, ParseException e) {
-                if (e != null) {
-                    Log.e("Homefragment", "Issue with getting posts", e);
-                    return;
+            public void done(List<ParseObject> objects, ParseException e) {
+                for (ParseObject o : objects) {
+                    Log.i("HomeFragment", "the following user is " + o.getString("username"));
+                    usernames.add((ParseUser) o);
                 }
-                for (Post post : postList) {
-                    Log.i("Homefragment", "Post " + post.getDescription() +  ", username " + post.getAuthor().getUsername() + " ," + post.getPrice());
+                for (ParseUser user : usernames) {
+                    followingUsersPosts.add(ParseQuery.getQuery(Post.class).whereEqualTo(Post.KEY_AUTHOR, user));
                 }
-                adapter.clear();
-                adapter.addAll(postList);
+                followingUsersPosts.add(ParseQuery.getQuery(Post.class).whereEqualTo(Post.KEY_AUTHOR, ParseUser.getCurrentUser()));
+                ParseQuery<Post> query = ParseQuery.or(followingUsersPosts);
+                query.include(Post.KEY_AUTHOR);
+                query.setLimit(20);
+                query.addDescendingOrder(Post.KEY_CREATED_AT);
+                query.findInBackground(new FindCallback<Post>() {
+                    @Override
+                    public void done(List<Post> postList, ParseException e) {
+                        if (e != null) {
+                            Log.e("Homefragment", "Issue with getting posts", e);
+                            return;
+                        }
+                        for (Post post : postList) {
+                            Log.i("Homefragment", "Post " + post.getDescription() +  ", username " + post.getAuthor().getUsername() + " ," + post.getPrice());
+                        }
+                        adapter.clear();
+                        adapter.addAll(postList);
+                    }
+                });
             }
         });
     }
