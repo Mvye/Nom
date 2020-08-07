060c412780653f19799dcb1c1d7eb5f459430696
Mervyn Mathew <mmervyn@fb.com>
2020-08-07 19:03:33 -0400

    Can now follow/unfollow users after clicking on their profile picture

--- app/src/main/java/com/mervynm/nom/fragments/ProfileFragment.java
+++ app/src/main/java/com/mervynm/nom/fragments/ProfileFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,9 +20,13 @@ import com.mervynm.nom.R;
 import com.mervynm.nom.models.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -29,8 +34,10 @@ public class ProfileFragment extends HomeFragment {
 
     ImageView imageViewProfilePicture;
     TextView textViewUsername;
+    Button buttonFollow;
     RecyclerView recyclerViewPosts;
     ParseUser userOfProfile;
+    List<String> followingUsers;
 
     public ProfileFragment() {}
 
@@ -57,7 +64,61 @@ public class ProfileFragment extends HomeFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         setupVariables(view);
+        setFollowOnClick();
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void setFollowOnClick() {
+        buttonFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateFollowingAndButton(true);
+            }
+        });
+    }
+
+    private void updateFollowingAndButton(final boolean pressedButton) {
+        buttonFollow.setClickable(false);
+        followingUsers = new ArrayList<>();
+        final ParseRelation<ParseUser> following = ParseUser.getCurrentUser().getRelation("following");
+        if (pressedButton && buttonFollow.getText().equals("Unfollow")) {
+            following.remove(userOfProfile);
+        }
+        else if (pressedButton && buttonFollow.getText().equals("Follow")){
+            following.add(userOfProfile);
+        }
+        if (pressedButton) {
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            currentUser.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    updateButton(following);
+                }
+            });
+        }
+        else {
+            updateButton(following);
+        }
+    }
+
+    private void updateButton(ParseRelation<ParseUser> following) {
+        final ParseQuery<ParseUser> followingList = following.getQuery();
+        followingList.include("User");
+        followingList.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                for (ParseUser user : users) {
+                    followingUsers.add(user.getObjectId());
+                }
+                if (followingUsers.contains(userOfProfile.getObjectId())) {
+                    buttonFollow.setText(R.string.unfollow);
+                }
+                else {
+                    buttonFollow.setText(R.string.follow);
+                }
+                buttonFollow.setClickable(true);
+            }
+        });
     }
 
     @Override
@@ -66,11 +127,19 @@ public class ProfileFragment extends HomeFragment {
                 .transform(new CircleCrop())
                 .into(imageViewProfilePicture);
         textViewUsername.setText(userOfProfile.getUsername());
+        if (userOfProfile.getUsername().equals(ParseUser.getCurrentUser().getUsername())) {
+            buttonFollow.setVisibility(View.GONE);
+        }
+        else {
+            buttonFollow.setVisibility(View.VISIBLE);
+            updateFollowingAndButton(false);
+        }
     }
 
     public void setupVariables(View view) {
         imageViewProfilePicture = view.findViewById(R.id.imageViewProfilePicture);
         textViewUsername = view.findViewById(R.id.textViewUsername);
+        buttonFollow = view.findViewById(R.id.buttonFollow);
         recyclerViewPosts = view.findViewById(R.id.recyclerViewPosts);
     }
 
